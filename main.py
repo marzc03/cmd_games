411a6dac1592ec8f50352f06e34c5fa5b46b8f6a
marzc03 <marzbanc26@gmail.com>
2026-05-30 00:03:53 -0400

    Started starfall Basically made title screen

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import random
 import time
 from rockpaperscissors import rock_paper_scissors
 from tictactoe import tic_tac_toe
+from starfall import starfall
 def start ():
     print("What game would you like to play?")
-    print(" 1) ROCK PAPER SCISSORS \n 2) TIC TAC TOE \n 3) RANDOM")
+    print(" 1) ROCK PAPER SCISSORS \n 2) TIC TAC TOE \n 3) STARFALL \n 4) RANDOM")
     resp = int(input())
     if resp == 1:
         rock_paper_scissors()
     if resp == 2:
         tic_tac_toe()
     if resp == 3:
+        starfall()
+    if resp == 4:
         print("Oh risky")
         print("Well...")
         print("3")
@@ -19,11 +22,13 @@ def start ():
         time.sleep(1)
         print("1")
         time.sleep(1)
-        resp = random.randint(1,2)
+        resp = random.randint(1,3)
         if resp == 1:
             rock_paper_scissors()
         if resp == 2:
             tic_tac_toe()
+        if resp == 3:
+            starfall()
     print("Play a different game? Y/N")
     resp = input()
     if resp == "Y" or resp == "y":
